013b0b02521ad7741a631552c391d29b7ee3fdb9
Travis J. Webb <me@traviswebb.com>
2017-03-18 22:48:01 -0400

    [cleanup] remove old test files

--- test/old/user-package/dummy1.js
+++
@@ -1,8 +0,0 @@
-(function(){
-var uax_full = require("uax11");
-
-module.exports = function(it) { return it };
-
-module.exports.uax_full = uax_full;
-
-}).call(this);
--- test/old/user-package/dummy2.js
+++
@@ -1,8 +0,0 @@
-(function(){
-var plv8x = require("plv8x");
-
-module.exports = function(it) { return it };
-
-module.exports.xpressionToBody = plv8x.xpressionToBody;
-
-}).call(this);
